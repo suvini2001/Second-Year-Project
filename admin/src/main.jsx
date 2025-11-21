5962a68b286eced7fae7b7dc18691ee4c9d9e6ec
suvini2001 <e21140@eng.pdn.ac.lk>
2025-11-21 19:17:16 +0530

    small update

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app.jsx'
+import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './contexts/adminContext.jsx'
 import DoctorContextProvider from './contexts/doctorContext.jsx'
