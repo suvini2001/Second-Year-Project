541dcf9dd1e48584c89ffa9c329f1361e6749c15
suvini2001 <e21140@eng.pdn.ac.lk>
2025-11-21 18:34:10 +0530

    name change

--- frontend/src/pages/LoginTemp.jsx.jsx
+++ frontend/src/pages/LoginTemp.jsx.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const LoginTemp = () => {
 
   const {backendUrl,token,setToken} = useContext(AppContext);
   const navigate = useNavigate();
@@ -95,6 +95,6 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default LoginTemp;
 
-//name updated from login to Login
+//name updated from login to LoginTemp.jsx
